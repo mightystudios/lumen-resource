233a2894b37c8c1439e0b0c90bd99e7aa868f4da
Fred Myerscough <oniice@gmail.com>
2016-09-04 14:57:01 +0100

    fix: typo in Lumen parameter, should be uses not use

--- src/ResourceRoute.php
+++ src/ResourceRoute.php
@@ -13,23 +13,23 @@ function resource($path, $controller, $name, $exclude = [])
     global $app;
 
     if (!(in_array('index', $exclude))) {
-        $app->get($path, ['as' => $name . '.index', 'use' => $controller . '@index']);
+        $app->get($path, ['as' => $name . '.index', 'uses' => $controller . '@index']);
     }
 
     if (!(in_array('show', $exclude))) {
-        $app->get($path . '/{id}', ['as' => $name . '.show', 'use' => $controller . '@show']);
+        $app->get($path . '/{id}', ['as' => $name . '.show', 'uses' => $controller . '@show']);
     }
 
     if (!(in_array('store', $exclude))) {
-        $app->post($path, ['as' => $name . '.store', 'use' => $controller . '@store']);
+        $app->post($path, ['as' => $name . '.store', 'uses' => $controller . '@store']);
     }
 
     if (!(in_array('update', $exclude))) {
-        $app->put($path . '/{id}', ['as' => $name . '.update', 'use' => $controller . '@update']);
+        $app->put($path . '/{id}', ['as' => $name . '.update', 'uses' => $controller . '@update']);
     }
 
     if (!(in_array('destroy', $exclude))) {
-        $app->delete($path . '/{id}', ['as' => $name . '.destroy', 'use' => $controller . '@destroy']);
+        $app->delete($path . '/{id}', ['as' => $name . '.destroy', 'uses' => $controller . '@destroy']);
     }
 
 }
